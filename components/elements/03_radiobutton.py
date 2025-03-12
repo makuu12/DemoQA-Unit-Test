b81b7aba757fff2051207d422b69c301d8fc16b9
shittypie <andaya.mark.roel.03122003@gmail.com>
2025-03-12 17:52:55 +0800

    add

---
+++ components/elements/03_radiobutton.py
@@ -0,0 +1,15 @@
+
+
+
+
+
+class raduobutton:
+    
+    def submitForm():
+        print("Click 'Yes' item")
+    
+    def submitForm():
+        print("Click 'Impressive' item")
+        
+    def submitForm():
+        print("Enable 'No' button then select")
---
+++ components/elements/04_webTables.py
@@ -0,0 +1,30 @@
+
+
+
+
+
+class webTables:
+    
+    def submitForm():
+        print("Click add and submit without data")
+    
+    def submitForm():
+        print("Click add and submit with valid data")
+        
+    def submitForm():
+        print("Click add and submit with invalid data")
+        
+    def submitForm():
+        print("Search from the list data")
+        
+    def submitForm():
+        print("Edit a data from the item")
+        
+    def submitForm():
+        print("Delete a data from the item")
+        
+    def submitForm():
+        print("Get all items from the table")
+
+    def submitForm():
+        print("Change number of rows")
