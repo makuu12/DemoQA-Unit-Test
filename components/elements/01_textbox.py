27dc6e1c97d0d7136291ef9725cf44c32bba97a8
shittypie <andaya.mark.roel.03122003@gmail.com>
2025-03-19 22:45:16 +0800

    change

--- components/elements/01_textbox.py
+++ components/elements/01_textbox.py
@@ -1,20 +1,9 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import time
 
 
 class textbox:
-    # -------------- Usage -------------- #
-    # from TC01_LogIn import Login
-    
-    # l = Login() 
-    # l.setup()           
-    # l.login()           
-    # l.close()  
-    # -------------- Usage -------------- #
-    
     def __init__(self):
         self.driver = None
         
@@ -28,26 +17,26 @@ class textbox:
     def fillUp_Form(self):
         vName = "mark"
         vEmail = "mark@gmail.com"
-        vCAdd = ""
-        vPAdd = ""
+        vCAdd = "test"
+        vPAdd = "test"
         self.driver.find_element(By.XPATH, "//input[@id='userName']").send_keys(vName)
         self.driver.find_element(By.XPATH, "//input[@id='userEmail']").send_keys(vEmail)
         self.driver.find_element(By.XPATH, "//textarea[@id='currentAddress']").send_keys(vCAdd)
         self.driver.find_element(By.XPATH, "//textarea[@id='permanentAddress']").send_keys(vPAdd)
-        self.driver.find_element(By.XPATH, "//button[@id='submit']").click()
+        time.sleep(5)
+        self.driver.find_element(By.XPATH, "(//button[normalize-space()='Submit'])[1]").click()
         
-        
+        time.sleep(5)
         main = self.driver.find_element(By.XPATH, "//div[@class='border col-md-12 col-sm-12']")
         name = main.find_element(By.XPATH, "//p[@id='name']")
-        # email = main.find_element(By.XPATH, "//p[@id='email']")
-        # cAdd = main.find_element(By.XPATH, "//p[@id='currentAddress']")
-        # pAdd = main.find_element(By.XPATH, "//p[@id='permanentAddress']")
+        email = main.find_element(By.XPATH, "//p[@id='email']")
+        cAdd = main.find_element(By.XPATH, "//p[@id='currentAddress']")
+        pAdd = main.find_element(By.XPATH, "//p[@id='permanentAddress']")
         
-        print(f"Name: {'PASSED' if name.text == 'Name:{vName}' else 'FALSE'}")
-        # print(f"Email: {'PASSED' if name.text == 'Name:{vEmail}' else 'FALSE'}")
-        # print("Email: PASSED" if email.text == "Email:sad" else "The text is not correct.")
-        # print("Email: PASSED" if cAdd.text == "Current Address:sad" else "The text is not correct.")
-        # print("Email: PASSED" if pAdd.text == "Permanent Address:sad" else "The text is not correct.")
+        print(f"Name: {'PASSED' if name.text == f'Name:{vName}' else 'FALSE'}")
+        print(f"Email: {'PASSED' if email.text == f'Name:{vEmail}' else 'FALSE'}")
+        print(f"Contact Address: {'PASSED' if cAdd.text == f'Name:{vCAdd}' else 'FALSE'}")
+        print(f"Permanent Address: {'PASSED' if pAdd.text == f'Name:{vPAdd}' else 'FALSE'}")
 
 
 if __name__ == "__main__":
