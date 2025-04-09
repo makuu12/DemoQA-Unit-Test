cfae5e27dbab5104097acd659b51741e3a30edea
shittypie <andaya.mark.roel.03122003@gmail.com>
2025-04-09 14:13:39 +0800

    Update 05_buttons.py

--- components/elements/05_buttons.py
+++ components/elements/05_buttons.py
@@ -13,16 +13,17 @@ class buttons:
         options.add_experimental_option("detach", True)
         self.driver = webdriver.Chrome(options=options)
         self.driver.maximize_window()
-        self.driver.get("https://https://demoqa.com/buttons")
+        self.driver.get("https://demoqa.com/buttons")
     
     def doubletClick(self): # Double click the button
-        button = self.driver.find_element(By.XPATH, "//button[@id='doubleClickBtn']")
+        button = self.driver.find_element(By.ID, "doubleClickBtn")
         
         actions = ActionChains(self.driver)
         actions.double_click(button).perform()
         
+        time.sleep(1)
         m = self.driver.find_element(By.XPATH, "//p[@id='doubleClickMessage']").text
-        print(f"Test 1: {'PASSED' if m == "You have done a double click" else 'FAILED'}")
+        print(f"Test 1: {'PASSED' if m == 'You have done a double click' else 'FAILED'}")
     
     def rightClick(self): # Right click the button
         button = self.driver.find_element(By.XPATH, "//button[@id='rightClickBtn']")
@@ -31,15 +32,13 @@ class buttons:
         actions.context_click(button).perform()
         
         m = self.driver.find_element(By.XPATH, "//p[@id='rightClickMessage']")
-        print("Right click the button")
-        
-        print(f"Test 1: {'PASSED' if m == "You have done a right click" else 'FAILED'}")
+        print(f"Test 1: {'PASSED' if m == 'You have done a right click' else 'FAILED'}")
         
     def normalClick(self): # Click the third button
-        button = self.driver.find_element(By.XPATH, "//button[@id='dFxvD']").click()
+        button = self.driver.find_element(By.XPATH, "(//button[normalize-space()='Click Me'])[1]").click()
         
         m = self.driver.find_element(By.XPATH, "//p[@id='dynamicClickMessage']")
-        print(f"Test 1: {'PASSED' if m == "You have done a dynamic click" else 'FAILED'}")
+        print(f"Test 1: {'PASSED' if m == 'You have done a dynamic click' else 'FAILED'}")
         
 if __name__ == "__main__":
     b = buttons()
