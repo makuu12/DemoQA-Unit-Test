57ff1b24cc7cccf469cb2353307c950eb72988d7
shittypie <andaya.mark.roel.03122003@gmail.com>
2025-05-31 19:29:32 +0800

    Updating directions

--- components/main.py
+++ components/main.py
@@ -1,3 +1,52 @@
 import time
 from selenium import webdriver
+from elements.TC01_textbox import textbox
+from elements.TC02_checkbox import checkbox
+from elements.TC03_radiobutton import raduobutton
+from elements.TC05_buttons import buttons
+from elements.TC06_links import links
+from elements.TC07_brokenLinkImages import linkImg
+from elements.TC08_File import DondownloadUpload 
+
+from selenium import webdriver
+
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=options)
+driver.maximize_window()
+driver.get("https://demoqa.com/text-box")
+# dapat demoqa lang then dapat ipupunta lang sa mga  menu
+
+
+
+t = textbox()
+t.setup()
+time.sleep(3)
+t.fillUp_Form()
+
+# Output:
+# Name:mark
+# Email:mark@gmail.com
+# Current Address :test
+# Permananet Address :test
+
+rb = raduobutton()
+rb.setup()
+time.sleep(4)
+rb.clickYes()
+time.sleep(4)
+rb.clickImpressive()
+time.sleep(4)
+rb.clickNo()
+
+
+
+
+
+
+
+
+
+
 
